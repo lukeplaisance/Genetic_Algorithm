0c7d0c171627d6ba1e7ebd296c7bae215dd4106d
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2019-01-09 16:05:43 -0600

    working on parsing through the text file

--- python_test.py
+++ python_test.py
@@ -1,25 +1,31 @@
 
+#class for holding the symbols, literals, and clauses for the expressions
 class Expression(object):
     def __init__(self, expression):
-        self.symbols = ["!", "+", "*", "(", ")"]
-        self.literals = []
-        self.variables = {}
+        self.symbols = ["!", "+", "*", "(", ")", " "]
+        
+        "(!a + c) * (!a + c + !e) * (!b + c + d + !e) * (a + !b + c) * (!e + f)"
         self.clauses = expression.split("*")
 
-    
+        self.literals = []
+        for character in expression:
+            if character not in self.symbols:
+                self.literals.append(character)
 
-class Parcer(Expression):
+        print (self.literals)
+
+    #presto = [a,b,c,1,0,1]
+#class for reading from a file
+class Parser(object):
     def __init__(self, filename):
-        self.file = open(filename, "r")
-        self.read_line = file.readline()
-        for self.read_line in file:
-            variables = clauses
+        self.file = filename        
+        self.open_file = open(self.file, "r")
+        self.lines = self.open_file.readlines()
+
+p = Parser("algorithm.txt")
+for line in p.lines:
+    e = Expression(line)
+        
+        
 
 
-
-
-
-
-file = open("algorithm.txt", "r")
-print(file.read())
-file.close
