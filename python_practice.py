eb43a7ef2c76e60daad171145db4e427abcb3a4b
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2019-01-17 11:47:50 -0600

    1/17/19
    morning practice problem

--- python_practice.py
+++ python_practice.py
@@ -9,17 +9,31 @@
 numbers = biggest_number(5, 2, 3)
 a = 1'''
 
-def common_numbers(listOne, listTwo):
+'''def common_numbers(listOne, listTwo):
     other_list = list()
     common_list = zip(listOne, listTwo)
     for item in common_list:
         if listOne[item] == listTwo[item]:
-            other_list.append(listTwo[item])
+            other_list.append(listTwo[item])'''
             
 
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+'''a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
 common_numbers(a, b)
-c = 2
-                 
+c = 2'''
+
+def even_numbers(numbers):
+    
+    new_numbers = []
+    for i in numbers:
+        if (i % 2 == 0):
+            i = 1
+        else:
+            i = 0
+        new_numbers.append(i)
+    return (new_numbers)
+
+a = [2, 3, 5, 6, 10, 11, 17]
+b = even_numbers(a)
+print (b) 
