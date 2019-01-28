c8b1b2720fbb0ae63e23e92b1b6277b281c896a3
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2019-01-28 15:20:12 -0600

    updated Gene class
    added more variables to the init function. Also added methods for generating a gene and determining the fitness score

--- gene.py
+++ gene.py
@@ -1,8 +1,23 @@
 import random
 
 class Gene():
-    def __init__(self, values):
-        self.values = values
+    def __init__(self, info):
+        self.info = str(info)
+        self.genes = []
+        self.fitness = 0
+
+    #generates a gene with randomly with numbers between 0 and 1
+    def gen_gene(self, size):
+        self.info = ""
+        for i in range(0, size, 1):
+            self.genes[i] = random.randint(0,1)
+            self.info += str(self.genes[i])
+
+    #determines the fitness score
+    def determine_fitness(self):
+        for value in self.genes:
+            if self.genes[value] == 1:
+                self.fitness += 1
 
     #gives each value in the gene a chance to flip it value
     def mutate(self, gene):
